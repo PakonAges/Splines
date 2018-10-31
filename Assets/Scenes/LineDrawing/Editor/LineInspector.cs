cfd21d203c3ae6d2b88bee36fbf6b78124786c60
Max Okorokov <maxim.okorokov@gmail.com>
2018-10-31 16:27:02 +0200

    SplineDrawing

---
+++ Assets/Scenes/LineDrawing/Editor/LineInspector.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor {
+
+    private void OnSceneGUI()
+    {
+        Line line = target as Line;
+
+        //Converting local points of the line -> World Space of handles
+        Transform handleTransform = line.transform;
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        Handles.color = Color.blue;
+        Handles.DrawLine(p0, p1);
+        
+        //Display Point of the line
+        Handles.DoPositionHandle(p0, handleRotation);
+        Handles.DoPositionHandle(p1, handleRotation);
+
+        //Make it possible to change Line in the Editor
+        EditorGUI.BeginChangeCheck();
+        p0 = Handles.DoPositionHandle(p0, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            //Make it possible to undo the changes
+            Undo.RecordObject(line, "Move Point");
+            //Make Unity know that something has changed
+            EditorUtility.SetDirty(line);
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+    }
+
+}
