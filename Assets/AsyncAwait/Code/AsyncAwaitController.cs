6475facdaeb27fda8a54e79dbdb6a85736e93044
Max Okorokov <maxim.okorokov@gmail.com>
2018-12-25 23:33:23 +0200

    Async-await SceneSetup

---
+++ Assets/AsyncAwait/Code/AsyncAwaitController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsyncAwaitTryouts
+{
+    public class AsyncAwaitController : MonoBehaviour
+    {
+        [Header("Setup")]
+        public int ItemsToSpawn = 8;
+        public GameObject Item;
+        public KeyCode SpawnKey = KeyCode.S;
+        public KeyCode ToggleMovement =  KeyCode.D;
+
+        [Header("Result")]
+        private bool _alreadyCreated = false;
+        private bool _isMoving = false;
+        public GameObject[] Items;
+
+        private void Awake()
+        {
+            Items = new GameObject[ItemsToSpawn];
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(SpawnKey))
+            {
+                if (!_alreadyCreated)
+                {
+                    SpawnItems();
+                }
+            }
+            else if (Input.GetKeyDown(ToggleMovement))
+            {
+                if (!_isMoving)
+                {
+                    StartMovement();
+                }
+                else
+                {
+                    StopMovement();
+                }
+            }
+        }
+
+        void SpawnItems()
+        {
+            for (int i = 0; i < ItemsToSpawn; i++)
+            {
+                Vector3 Pos = new Vector3(i, 0, 0);
+                var newItem = Instantiate(Item,Pos,Quaternion.identity);
+                Items[i] = newItem;
+            }
+
+            _alreadyCreated = true;
+        }
+
+        void StartMovement()
+        {
+            _isMoving = true;
+            Debug.Log("Movement Started");
+        }
+
+        void StopMovement()
+        {
+            _isMoving = false;
+            Debug.Log("Movement Stopped");
+        }
+    }
+}
