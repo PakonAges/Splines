aff7cfd09fb3735418aa5061b678248f760effdd
Max Okorokov <maxim.okorokov@gmail.com>
2019-01-19 23:03:18 +0200

    Prefab provider

---
+++ Assets/UISystem/UI.Scripts/Interfaces/IUiPrefabProvider.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public interface IUiPrefabProvider
+{
+    //TO DO: make normal method and do adapter/decorator to implement async if needed
+    Task<GameObject> GetPrefabAsync(ViewType view);
+}
---
+++ Assets/UISystem/UI.Scripts/PrefabProviders/AdressablePrefabProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AdressablePrefabProvider : IUiPrefabProvider
+{
+    public async Task<GameObject> GetPrefabAsync(ViewType type)
+    {
+        if (type == ViewType.Invalid)
+        {
+            Debug.LogError("Trying to get Invalid UI Prefab");
+            return null;
+        }
+        else
+        {
+            var window = await Addressables.LoadAsset<GameObject>(type.ToString()) as GameObject;
+            return window;
+        }
+
+        throw new System.NotImplementedException();
+    }
+}
